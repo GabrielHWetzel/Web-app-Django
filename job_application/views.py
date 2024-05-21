b78269b6308b3564e69365c3cc0a1dada888eabe
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-05-21 10:55:02 -0300

    Created form model and receiving post data

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
     return render(request, "index.html")

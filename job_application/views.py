e9ea53aea13109b4c19fd7bcb96f3348d96b5149
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-05-23 09:42:46 -0300

    Configured email sending

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import ContactForm
+from .models import Form
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -12,4 +15,17 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name,
+                                last_name=last_name,
+                                email=email,
+                                date=date,
+                                occupation=occupation)
+            message_body = f"New job application was submitted. {first_name}"
+            email_message = EmailMessage("Form submission confirmation",
+                                         message_body,
+                                         to=[email])
+            email_message.send()
+
+            messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+email = os.getenv("EMAIL")
+EMAIL_HOST_USER = email
+pswrd = os.getenv("EMAIL_PASSWORD")
+EMAIL_HOST_PASSWORD = pswrd
